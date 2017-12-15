f16a13eba5dc8399f3f2560788da93725c6e4eeb
Alan Shaw <alan@tableflip.io>
2017-12-15 11:52:23 +0000

    adds remaining easy API calls

--- src/client.js
+++ src/client.js
@@ -2,7 +2,7 @@ const { caller } = require('postmsg-rpc')
 const callbackify = require('callbackify')
 
 module.exports = (opts) => {
-  return {
+  const ipfs = {
     id: callbackify(caller('ipfs.id', opts)),
     version: callbackify(caller('ipfs.version', opts)),
     block: {
@@ -27,6 +27,50 @@ module.exports = (opts) => {
       provide: callbackify.variadic(caller('ipfs.dht.provide', opts)),
       query: callbackify.variadic(caller('ipfs.dht.query', opts))
     },
+    files: {
+      add: callbackify.variadic(caller('ipfs.files.add', opts)),
+      // addReadableStream: callbackify.variadic(caller('ipfs.files.addReadableStream', opts)),
+      // addPullStream: callbackify.variadic(caller('ipfs.files.addPullStream', opts)),
+      cat: callbackify.variadic(caller('ipfs.files.cat', opts)),
+      // catReadableStream: callbackify.variadic(caller('ipfs.files.catReadableStream', opts)),
+      // catPullStream: callbackify.variadic(caller('ipfs.files.catPullStream', opts)),
+      get: callbackify.variadic(caller('ipfs.files.get', opts))
+    },
+    ls: callbackify.variadic(caller('ipfs.ls', opts)),
+    // lsReadableStream: callbackify.variadic(caller('ipfs.files.catReadableStream', opts)),
+    // lsPullStream: callbackify.variadic(caller('ipfs.files.catPullStream', opts)),
+    key: {
+      gen: callbackify.variadic(caller('ipfs.key.gen', opts)),
+      list: callbackify(caller('ipfs.key.list', opts)),
+      rename: callbackify.variadic(caller('ipfs.key.rename', opts)),
+      rm: callbackify.variadic(caller('ipfs.key.rm', opts))
+    },
+    object: {
+      new: callbackify.variadic(caller('ipfs.object.new', opts)),
+      put: callbackify.variadic(caller('ipfs.object.put', opts)),
+      get: callbackify.variadic(caller('ipfs.object.get', opts)),
+      data: callbackify.variadic(caller('ipfs.object.data', opts)),
+      links: callbackify.variadic(caller('ipfs.object.links', opts)),
+      stat: callbackify.variadic(caller('ipfs.object.stat', opts)),
+      patch: {
+        addLink: callbackify.variadic(caller('ipfs.object.patch.addLink', opts)),
+        rmLink: callbackify.variadic(caller('ipfs.object.patch.rmLink', opts)),
+        appendData: callbackify.variadic(caller('ipfs.object.patch.appendData', opts)),
+        setData: callbackify.variadic(caller('ipfs.object.patch.setData', opts))
+      }
+    },
+    pin: {
+      add: callbackify.variadic(caller('ipfs.pin.add', opts)),
+      rm: callbackify.variadic(caller('ipfs.pin.rm', opts)),
+      ls: callbackify.variadic(caller('ipfs.pin.ls', opts))
+    },
+    pubsub: {
+      publish: callbackify.variadic(caller('ipfs.pubsub.publish', opts)),
+      // subscribe: callbackify.variadic(caller('ipfs.pubsub.subscribe', opts)),
+      // unsubscribe: callbackify.variadic(caller('ipfs.pubsub.unsubscribe', opts)),
+      peers: callbackify.variadic(caller('ipfs.pubsub.peers', opts)),
+      ls: callbackify.variadic(caller('ipfs.pubsub.ls', opts))
+    },
     swarm: {
       peers: callbackify.variadic(caller('ipfs.swarm.peers', opts)),
       addrs: callbackify(caller('ipfs.swarm.addrs', opts)),
@@ -35,4 +79,9 @@ module.exports = (opts) => {
       disconnect: callbackify.variadic(caller('ipfs.swarm.disconnect', opts))
     }
   }
+
+  // Aliases
+  ipfs.add = ipfs.files.add
+
+  return ipfs
 }
--- src/server.js
+++ src/server.js
@@ -26,6 +26,42 @@ module.exports = (getIpfs, opts) => {
       provide: expose('ipfs.dht.provide', (...args) => getIpfs().dht.provide(...args), opts),
       query: expose('ipfs.dht.query', (...args) => getIpfs().dht.query(...args), opts)
     },
+    files: {
+      add: expose('ipfs.files.add', (...args) => getIpfs().files.add(...args), opts),
+      cat: expose('ipfs.files.cat', (...args) => getIpfs().files.cat(...args), opts),
+      get: expose('ipfs.files.get', (...args) => getIpfs().files.get(...args), opts)
+    },
+    ls: expose('ipfs.ls', (...args) => getIpfs().ls(...args), opts),
+    key: {
+      gen: expose('ipfs.key.gen', (...args) => getIpfs().key.gen(...args), opts),
+      list: expose('ipfs.key.list', () => getIpfs().key.list(), opts),
+      rename: expose('ipfs.key.rename', (...args) => getIpfs().key.rename(...args), opts),
+      rm: expose('ipfs.key.rm', (...args) => getIpfs().key.rm(...args), opts)
+    },
+    object: {
+      new: expose('ipfs.object.new', (...args) => getIpfs().object.new(...args), opts),
+      put: expose('ipfs.object.put', (...args) => getIpfs().object.put(...args), opts),
+      get: expose('ipfs.object.get', (...args) => getIpfs().object.get(...args), opts),
+      data: expose('ipfs.object.data', (...args) => getIpfs().object.data(...args), opts),
+      links: expose('ipfs.object.links', (...args) => getIpfs().object.links(...args), opts),
+      stat: expose('ipfs.object.stat', (...args) => getIpfs().object.stat(...args), opts),
+      patch: {
+        addLink: expose('ipfs.object.patch.addLink', (...args) => getIpfs().object.patch.addLink(...args), opts),
+        rmLink: expose('ipfs.object.patch.rmLink', (...args) => getIpfs().object.patch.rmLink(...args), opts),
+        appendData: expose('ipfs.object.patch.appendData', (...args) => getIpfs().object.patch.appendData(...args), opts),
+        setData: expose('ipfs.object.patch.setData', (...args) => getIpfs().object.patch.setData(...args), opts)
+      }
+    },
+    pin: {
+      add: expose('ipfs.pin.add', (...args) => getIpfs().pin.add(...args), opts),
+      rm: expose('ipfs.pin.rm', (...args) => getIpfs().pin.rm(...args), opts),
+      ls: expose('ipfs.pin.ls', (...args) => getIpfs().pin.ls(...args), opts)
+    },
+    pubsub: {
+      publish: expose('ipfs.pubsub.publish', (...args) => getIpfs().pubsub.publish(...args), opts),
+      peers: expose('ipfs.pubsub.peers', () => getIpfs().pubsub.peers(), opts),
+      ls: expose('ipfs.pubsub.ls', (...args) => getIpfs().pubsub.ls(...args), opts)
+    },
     swarm: {
       peers: expose('ipfs.swarm.peers', (...args) => getIpfs().swarm.peers(...args), opts),
       addrs: expose('ipfs.swarm.addrs', () => getIpfs().swarm.addrs(), opts),
