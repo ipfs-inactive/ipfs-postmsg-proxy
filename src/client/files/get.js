bcefe83ffa782eab6824303aef491c0af95c8e35
Alan Shaw <alan@tableflip.io>
2018-01-27 22:07:05 +0000

    implements getPullStream server side

--- src/client/files/get.js
+++ src/client/files/get.js
@@ -59,6 +59,7 @@ export default function (opts) {
                   }
                 }))
               }
+
               return file
             })
           )
--- src/server/files/get.js
+++ src/server/files/get.js
@@ -1,7 +1,11 @@
 import { expose } from 'postmsg-rpc'
 import { pre, post } from 'prepost'
+import pull from 'pull-stream'
+import PMS from 'pull-postmsg-stream'
+import shortid from 'shortid'
 import { preCidFromJson } from '../../serialization/cid'
-import { preBufferFromJson, bufferToJson } from '../../serialization/buffer'
+import { isBuffer, preBufferFromJson, bufferToJson } from '../../serialization/buffer'
+import { functionToJson } from '../../serialization/function'
 
 export default function (getIpfs, opts) {
   return {
@@ -17,6 +21,46 @@ export default function (getIpfs, opts) {
           }
 
           return file
+        })
+      )
+    ), opts),
+    getPullStream: expose('ipfs.files.getPullStream', pre(
+      preBufferFromJson(0),
+      preCidFromJson(0),
+      opts.pre['files.getPullStream'],
+      post(
+        (...args) => getIpfs().files.getPullStream(...args),
+        (res) => new Promise(resolve => {
+          const readFnName = shortid()
+
+          pull(
+            res,
+            pull.map(file => {
+              if (file.content) {
+                const readFnName = shortid()
+
+                pull(
+                  file.content,
+                  PMS.sink(readFnName, Object.assign({}, opts, {
+                    post (res) {
+                      if (isBuffer(res.data)) {
+                        res.data = bufferToJson(res.data)
+                      }
+
+                      return res
+                    }
+                  }))
+                )
+
+                file.content = functionToJson(readFnName)
+              }
+
+              return file
+            }),
+            PMS.sink(readFnName, opts)
+          )
+
+          resolve(functionToJson(readFnName))
         })
       )
     ), opts)
