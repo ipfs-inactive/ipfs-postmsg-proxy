99ab386cc6103232b185a0f4e184102e286a91fa
Alan Shaw <alan@tableflip.io>
2018-02-12 10:56:33 +0000

    adds missing pre to files.mkdir

--- src/server/files/mkdir.js
+++ src/server/files/mkdir.js
@@ -1,7 +1,11 @@
 import { expose } from 'postmsg-rpc'
+import { pre } from 'prepost'
 
 export default function (getIpfs, opts) {
   return {
-    mkdir: expose('ipfs.files.mkdir', (...args) => getIpfs().files.mkdir(...args), opts)
+    mkdir: expose('ipfs.files.mkdir', pre(
+      opts.pre('files.mkdir'),
+      (...args) => getIpfs().files.mkdir(...args)
+    ), opts)
   }
 }
