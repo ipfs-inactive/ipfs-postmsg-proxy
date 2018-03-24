21ecd2b86a589b607d8892b0de8f795ddcceb606
Alan Shaw <alan@tableflip.io>
2018-03-24 21:04:00 +0000

    safter bitswap stat

--- src/client/bitswap.js
+++ src/client/bitswap.js
@@ -1,21 +1,41 @@
 import { caller } from 'postmsg-rpc'
 import callbackify from 'callbackify'
 import { post } from 'prepost'
-import { bigFromJson } from '../serialization/big'
+import { isBigJson, bigFromJson } from '../serialization/big'
 
 export default function (opts) {
   return {
     stat: callbackify(
       post(
         caller('ipfs.bitswap.stat', opts),
-        (res) => {
-          res.blocksReceived = bigFromJson(res.blocksReceived)
-          res.dataReceived = bigFromJson(res.dataReceived)
-          res.blocksSent = bigFromJson(res.blocksSent)
-          res.dataSent = bigFromJson(res.dataSent)
-          res.dupBlksReceived = bigFromJson(res.dupBlksReceived)
-          res.dupDataReceived = bigFromJson(res.dupDataReceived)
-          return res
+        (stats) => {
+          if (stats) {
+            if (isBigJson(stats.blocksReceived)) {
+              stats.blocksReceived = bigFromJson(stats.blocksReceived)
+            }
+
+            if (isBigJson(stats.dataReceived)) {
+              stats.dataReceived = bigFromJson(stats.dataReceived)
+            }
+
+            if (isBigJson(stats.blocksSent)) {
+              stats.blocksSent = bigFromJson(stats.blocksSent)
+            }
+
+            if (isBigJson(stats.dataSent)) {
+              stats.dataSent = bigFromJson(stats.dataSent)
+            }
+
+            if (isBigJson(stats.dupBlksReceived)) {
+              stats.dupBlksReceived = bigFromJson(stats.dupBlksReceived)
+            }
+
+            if (isBigJson(stats.dupDataReceived)) {
+              stats.dupDataReceived = bigFromJson(stats.dupDataReceived)
+            }
+          }
+
+          return stats
         }
       )
     )
--- src/server/bitswap.js
+++ src/server/bitswap.js
@@ -1,19 +1,39 @@
 import { expose } from 'postmsg-rpc'
 import { post } from 'prepost'
-import { bigToJson } from '../serialization/big'
+import { isBig, bigToJson } from '../serialization/big'
 
 export default function (getIpfs, opts) {
   return {
     stat: expose('ipfs.bitswap.stat', post(
       (...args) => getIpfs().bitswap.stat(...args),
-      (res) => {
-        res.blocksReceived = bigToJson(res.blocksReceived)
-        res.dataReceived = bigToJson(res.dataReceived)
-        res.blocksSent = bigToJson(res.blocksSent)
-        res.dataSent = bigToJson(res.dataSent)
-        res.dupBlksReceived = bigToJson(res.dupBlksReceived)
-        res.dupDataReceived = bigToJson(res.dupDataReceived)
-        return res
+      (stats) => {
+        if (stats) {
+          if (isBig(stats.blocksReceived)) {
+            stats.blocksReceived = bigToJson(stats.blocksReceived)
+          }
+
+          if (isBig(stats.dataReceived)) {
+            stats.dataReceived = bigToJson(stats.dataReceived)
+          }
+
+          if (isBig(stats.blocksSent)) {
+            stats.blocksSent = bigToJson(stats.blocksSent)
+          }
+
+          if (isBig(stats.dataSent)) {
+            stats.dataSent = bigToJson(stats.dataSent)
+          }
+
+          if (isBig(stats.dupBlksReceived)) {
+            stats.dupBlksReceived = bigToJson(stats.dupBlksReceived)
+          }
+
+          if (isBig(stats.dupDataReceived)) {
+            stats.dupDataReceived = bigToJson(stats.dupDataReceived)
+          }
+        }
+
+        return stats
       }
     ), opts)
   }
